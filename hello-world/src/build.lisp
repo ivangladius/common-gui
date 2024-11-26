d47fd289f759bda0a2885e8dbde1adcc23610368
Maximilian Ivan Filipov <maximilian.filipov@proton.me>
2024-11-26 09:07:40 +0100

    Revert "create raylib bindings now creating a GUI framework"
    This reverts commit 0be30fd76cfe3a7a37c5b94ab660e8e9aa0e7728.

--- hello-world/src/build.lisp
+++
@@ -1,7 +0,0 @@
-
-(defun build-raylib ()
-  (let ((raylib-url "https://github.com/raysan5/raylib")
-        (make-command "make PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=SHARED -j$(nproc)"))
-    (unless (probe-file #P"raylib")
-      (uiop:run-program (format nil "git clone ~a" raylib-url) :output t))
-    (uiop:run-program (format nil "bash -c 'cd raylib/src && ~a'" make-command) :output t)))
